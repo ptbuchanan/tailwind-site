a2ccb338d250380876cfb427c7f750350db0f739
ptbuchanan <ptbuchanan@loyola.edu>
2023-08-31 17:46:37 -0400

    Made progress on hero

--- tails/modernsite/src/App.js
+++ tails/modernsite/src/App.js
@@ -1,46 +1,33 @@
-import React from 'react'
-import styles from './styles'
-
+import styles from "./style";
 import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
-const App = () => {
-  return (
-    // Div will be wrapping our entire app
-    <div className="bg-primary w-full overflow-hidden">
-      {/* Will wrap the nav bar and give it padding*/}
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-         {/* Will style the width full*/}
-        <div className={`${styles.boxWidth}`}>  
-          <Navbar/>
-        </div>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
-
-      {/* Will make hero section now*/}
-      <div className={`bg-primary ${styles.flexStart}`}>
-        {/* Styling the box */}
-        <div className={`${styles.boxWidth}`}>
-          <Hero/>
-        </div>
-      </div>
-
-      {/* Will make  section now*/}
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        {/* Styling the box */}
-        <div className={`${styles.boxWidth}`}>
-          <Stats/>
-          <Business/>
-          <Billing/>
-          <CardDeal/>
-          <Testimonials/>
-          <Clients/>
-          <CTA/>
-          <Footer/>
-        </div>
-      </div>
-
-
     </div>
-  )
-}
 
-export default App
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
